6b8f3d2d3e988b4ea4c3d2e0a080acc555e625c9
Fire-Mind <52949382+Fire-Mind@users.noreply.github.com>
2019-07-16 11:02:19 -0400

    第一次

---
+++ sendMail.py
@@ -0,0 +1,75 @@
+import smtplib
+import json
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+my_name='YourName'
+my_mail='YourEmail'
+my_pw="YourPassWord"
+source='localization.json'
+templateDem='templateDem.txt'
+templateGop='templateGop.txt'
+
+def sendMail(target,content,receiver):
+	try:
+		msg=MIMEText(content,'plain','utf-8')
+		msg['From']=formataddr([my_name,my_mail])
+		msg['To']=formataddr([receiver,target])
+		msg['Subject']='My concern regarding S.386'
+
+		server=smtplib.SMTP('smtp.office365.com',587)#This depends on the Email you are using
+		server.starttls()
+		'''
+		If you are using gmail:
+		server = smtplib.SMTP(smtp.gmail.com, 587)
+		Also you need to enable SMTP service of your Email first
+		'''
+		server.login(my_mail,my_pw)
+		server.sendmail(my_mail,[target,],msg.as_string())
+
+		server.quit()
+		print("Mail delivered to: "+target)
+		return True
+	except e:
+		print(str(e))
+		print("Failed to deliver: "+target)
+		return False
+		
+
+
+def parseJson():
+	dems=[]
+	gops=[]
+	inputFile=open(source)
+	obj=json.load(inputFile)
+	for rep in obj['Schedulers 2019']:
+		if 'Last' in rep and rep['Legislator Title']=='Senator':
+			repItem={'name':rep['Last'],'mail':rep['Email']}
+			if rep['Party']=='R':
+				gops.append(repItem)
+			else:
+				dems.append(repItem)
+	return dems,gops
+
+
+def createContent(template):
+	inputFile=open(template,encoding="utf8")
+	content=inputFile.read()
+	content=content+'\n'+my_name
+	return content
+
+
+	
+dems,gops=parseJson()
+demContent=createContent(templateDem)
+gopContent=createContent(templateGop)
+for rep in dems:
+	demContent='Dear senator ' + rep['name'] + ',\n' +demContent
+	sendMail(rep['mail'],demContent,rep['name'])
+for rep in gops:
+	gopContent='Dear senator ' + rep['name'] + ',\n' +gopContent
+	sendMail(rep['mail'],gopContent,rep['name'])
+print('done')	
+
+
+	
